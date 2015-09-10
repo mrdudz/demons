51864a7d0622f184d69b217342212cf9024fd037
Petri Hakkinen <petrih3@gmail.com>
2015-09-10 11:12:20 +0300

    Very simple music player. Need to compress music data.

--- main.asm
+++ main.asm
@@ -125,8 +125,8 @@ reveal_dx	= $28
 reveal_dy 	= $29
 reveal_tmp	= $30
 damage_char	= $31		; temp for damage flash
-; these are updated by kernal's PLOT routine
-LINE_PTR	= $d1		; pointer to current line is stored in $d1-$d2
+music_pos	= $32		; $32-$33 = current music position
+LINE_PTR	= $d1		; $d1-$d2 pointer to current line (updated by Kernal)
 CURSOR_X	= $d3
 CURSOR_Y	= $d6
 
--- music.asm
+++ music.asm
@@ -1,8 +1,15 @@
+TEMPO = 40
+
 	;*****************************************************************
 	; init music
 	;*****************************************************************
 
 init_music:
+	lda #0			; reset music position
+	sta music_pos
+	sta music_pos+1
+	lda #15			; set music volume
+	sta $900e
 	sei
 	lda #<irq
 	sta $0314
@@ -15,7 +22,40 @@ init_music:
 	; music interrupt handler
 	;*****************************************************************
 
-irq:	; TODO: implement music playback here
-	inc $900f
+irq:	lda music_pos		; increment music pos
+	clc
+	adc #TEMPO 
+	sta music_pos
+	bcc @skip
+	inc music_pos+1
+@skip:
+
+	; pattern position
+	lda music_pos+1
+	and #31
+	tax
+
+	; play note
+	lda bass,x
+	sta $900a
+	lda alto,x
+	sta $900b
+	lda soprano,x
+	sta $900c
+	lda noise,x
+	sta $900d
+
+	;inc $900f
 	jmp $eabf
 
+bass:	.byte $9c,$00,$00,$00,$9c,$00,$9c,$00,$00,$00,$9c,$00,$9c,$00,$00,$00
+	.byte $9c,$00,$9c,$00,$00,$00,$9c,$00,$9c,$00,$00,$00,$9c,$00,$9c,$00
+
+alto:	.byte $9c,$00,$9c,$00,$bd,$00,$00,$00,$00,$00,$9c,$00,$bd,$00,$b5,$00
+	.byte $9c,$00,$9c,$00,$bd,$00,$00,$00,$00,$00,$9c,$00,$bd,$00,$c1,$00
+
+soprano:.byte $bd,$00,$00,$bd,$00,$00,$bd,$00,$c1,$bd,$00,$00,$00,$00,$00,$00
+	.byte $00,$00,$00,$00,$00,$00,$00,$00,$9c,$00,$9c,$00,$a7,$00,$ac,$00
+
+noise:	.byte $02,$00,$00,$00,$e0,$00,$00,$00,$02,$00,$02,$00,$e0,$00,$00,$00
+	.byte $02,$00,$00,$00,$e0,$00,$00,$00,$02,$00,$02,$00,$e0,$00,$02,$00
